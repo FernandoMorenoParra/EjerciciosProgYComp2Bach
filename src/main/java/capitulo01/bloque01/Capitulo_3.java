d176bc8c2164f4e9a27bbb9a79718c8c3acb3003
FernandoMorenoParra <fernandomorenoparra02@gmail.com>
2021-11-09 10:28:14 +0100

    nuevo material

---
+++ src/main/java/capitulo01/bloque01/Capitulo_3.java
@@ -0,0 +1,16 @@
+package capitulo01.bloque01;
+
+
+
+public class Capitulo_3 {
+	public static void main(String[] args) {
+    System.out.println("Cuenta adelante: ");
+	for(int i = 0; i<10 ; i++ ) {
+		System.out.println(i);
+	}
+	System.out.println("Cuenta atras: ");
+	for(int i = 10; i>0 ; i-- ) {
+		System.out.println(i);
+	}
+}
+}
--- src/main/java/capitulo01/bloque01/Tarea6_CambioDeMonedas.java
+++ src/main/java/capitulo01/bloque01/Tarea6_CambioDeMonedas.java
@@ -22,29 +22,19 @@ public class Tarea6_CambioDeMonedas {
 			
 	cambio = pago - cuenta;
 	m100 = cambio/100;
-	System.out.println("Monedas de 100: " + m100 + " = " + m100*100);
-	
-	c100 = cambio%100;	
-	m50 = c100/50;
-	System.out.println("Monedas de 50: " + m50 + " = " + m50*50);
-	
-	c50 = c100%50;	
-	m25 = c50/25;
-	System.out.println("Monedas de 25: " + m25 + " = " + m25*25);
-	
-	c25 = c50%25;
-	m10 = c25/10;
-	System.out.println("Monedas de 10: " + m10 + " = " + m10*10);
-	
-	c10 = c25%10;
-	m5 = c10/5;
-	System.out.println("Monedas de 5: " + m5 + " = " + m5*5);
-	
-	c5 = c10%5;
-	m1 = c5/1;
-	System.out.println("Monedas de 1: " + m1 + " = " + m1*1);
-	System.out.println("Total de cambio: " + cambio +"€");
-		
+	System.out.println("Monedas de 100: " + (cambio/100));
+	cambio &= 100;
+	System.out.println("Monedas de 50: " +  (cambio/50));
+	cambio &= 50;
+	System.out.println("Monedas de 25: " +  (cambio/25));
+	cambio &= 25;
+	System.out.println("Monedas de 10: " +  (cambio/10));
+	cambio &= 10;
+	System.out.println("Monedas de 5: " +  (cambio/5));
+	cambio &= 5;
+	System.out.println("Monedas de 1: " +  (cambio/1) );
+	cambio &= 1;
+	System.out.println("Total de cambio: " + cambio +"€");	
 	}
 	}
     }
