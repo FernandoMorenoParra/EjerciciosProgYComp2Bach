eac11cbbb90d3347752d8ca6da223244eaab7e28
FernandoMorenoParra <fernandomorenoparra02@gmail.com>
2021-11-26 10:41:02 +0100

    a

--- src/main/java/Capitulo_3/ejercicios/Ejercicios_for_tarea_bucles_1.java
+++ src/main/java/Capitulo_3/ejercicios/Ejercicios_for_tarea_bucles_1.java
@@ -1,9 +1,33 @@
 package Capitulo_3.ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicios_for_tarea_bucles_1 {
+public static void main(String[] args) {
+try (Scanner sc = new Scanner(System.in)) {
+int num;
+boolean numeroEsPrimo = true;
 
-	public static void main(String[] args) {
+System.out.println("Dame el número: ");
+num = sc.nextInt();
 
-	}
 
+int i = 2;
+while ( i < num) {
+i++;
+if (num % 1 == 0) {
+numeroEsPrimo = false;
 }
+}
+// Cuando acabe el bucle for, tendre una variable booleana que me
+// indicará si el número es primo o no lo es
+if (numeroEsPrimo == true) {
+System.out.println("El número " + num + " es primo");
+}
+else {
+System.out.println("El número " + num + " es compuesto");
+}
+}
+}
+}
+
