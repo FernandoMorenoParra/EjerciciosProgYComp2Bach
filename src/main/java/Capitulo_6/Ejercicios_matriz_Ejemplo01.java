855ce696e00b613e97d262b6c33066dac30965dc
FernandoMorenoParra <fernandomorenoparra02@gmail.com>
2021-12-17 11:26:02 +0100

    17/12/21

---
+++ src/main/java/Capitulo_6/Ejercicios_matriz_Ejemplo01.java
@@ -0,0 +1,28 @@
+package Capitulo_6;
+
+public class Ejercicios_matriz_Ejemplo01 {
+
+	public static void main(String[] args) {
+		
+			// Declaración de una matriz de 3 x 3
+			int matriz[][] = new int[3][3];
+
+			// Inicializar los valores dentro de la matriz
+			for (int i = 0; i < matriz.length; i++) {
+				for (int j = 0; j < matriz[i].length; j++) {
+					matriz[i][j] = (int) Math.round(Math.random() * 100);
+				}
+			}
+			
+			System.out.println("Matriz inicializada");
+			
+			// Visualizar los elementos de la matriz en la consola
+			for (int i = 0; i < matriz.length; i++) {
+				for (int j = 0; j < matriz[i].length; j++) {
+					System.out.print(matriz[i][j] + " ");
+				}
+				System.out.println();
+			}
+		}
+
+	}
---
+++ src/main/java/Capitulo_6/Ejercicios_matriz_Ejemplo02.java
@@ -0,0 +1,40 @@
+package Capitulo_6;
+
+public class Ejercicios_matriz_Ejemplo02 {
+
+	public static void main(String[] args) {
+		// Declaración de una matriz de 3 x 3
+				int matriz[][] = new int[3][3];
+
+				inicializaMatriz(matriz);
+				
+				muestraMatriz(matriz);
+			}
+
+			/**
+			 * 
+			 * @param matriz
+			 */
+			public static void inicializaMatriz (int matriz[][]) {
+				// Inicializar los valores dentro de la matriz
+				for (int i = 0; i < matriz.length; i++) {
+					for (int j = 0; j < matriz[i].length; j++) {
+						matriz[i][j] = (int) Math.round(Math.random() * 100);
+					}
+				}		
+			}
+			
+			/**
+			 * 
+			 * @param matriz
+			 */
+			public static void muestraMatriz (int matriz[][]) {
+				// Visualizar los elementos de la matriz en la consola
+				for (int i = 0; i < matriz.length; i++) {
+					for (int j = 0; j < matriz[i].length; j++) {
+						System.out.print(matriz[i][j] + " ");
+					}
+					System.out.println();
+				}
+			}
+		}
