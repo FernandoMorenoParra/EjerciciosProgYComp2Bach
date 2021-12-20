acf4b5f1bba2ace03699a83376d15a323f3bcd78
Gamer <Gamer@GAMING>
2021-12-20 22:34:37 +0100

    20/12/2021

---
+++ src/main/java/Capitulo_6/Ejercicios_matriz_Tarea01.java
@@ -0,0 +1,43 @@
+package Capitulo_6;
+
+public class Ejercicios_matriz_Tarea01 {
+
+	public static void main(String[] args) {
+		int matriz[][] = new int[][] {  { 3, 5, 6, 8, 9 },
+			{10, 13, 16, 18, 19}, 
+			{21, 24, 26, 27, 29}, 
+			{31, 33, 35, 38, 39}, 
+			{42, 43, 44, 47, 49} };
+			
+}
+
+
+
+/**
+* 
+* @param matriz
+*/
+public static void mostrarMatriz (int matriz[][]) {
+for (int i = 0; i < matriz.length; i++) {
+for (int j = 0; j < matriz[i].length; j++) {
+System.out.print(matriz[i][j] + " ");
+}
+}
+}
+/**
+* 
+* @param matriz
+* @return
+*/
+public static boolean MatrizP (int matriz[][]) {
+boolean resultado = true;
+for (int i = 0; i < matriz.length; i++) {
+for (int j = 0; j < matriz[i].length; j++) {
+if (matriz[i][j] < 0) {
+resultado = false;
+}
+}
+}
+return resultado;
+}
+}
